7a6336c493fbb1dd045376a1145ae857f6f330b4
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-01-17 03:50:41 +0800

    update

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+// const mongoose = require("mongoose");
 let users = [];
-let message = [];
+let messages = [];
 let index = 0;
 
 io.on("connection", socket => {
-    
+	socket.emit('loggedIn', {
+		users: users.map(s => s.username),
+		messages: messages
+	});
 
+	socket.on('newuser', username => {
+		console.log(`${username} has join the chat.`);
+		socket.username = username;
+		users.push(socket);
 
+		io.emit('userOnline', socket.username);
+	});
 
+	socket.on('msg', msg => {
+		let message = new ChatModel({
+            index: index,
+			username: socket.username,
+			msg: msg
+        });
+        
+        messages.push(message);
 
-    
-    // Disconnect
-    socket.on("disconnect", () => {
-        console.log(`${socket.username} has left the chat.`);
-    });
+        io.emit('msg', message);
+
+        index++;
+	});
+	
+	// Disconnect
+	socket.on("disconnect", () => {
+		console.log(`${socket.username} has left the chat.`);
+		io.emit("userLeft", socket.username);
+		users.splice(users.indexOf(socket), 1);
+	});
 });
 
-
-
 http.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on port %s", process.env.PORT || 3000);
-})
+	console.log("Listening on port %s", process.env.PORT || 3000);
+});
